3825986bbddd61ac4bdaef7ad7317874be85b3f1
yuto343 <namawasabi1229@gmail.com>
2024-02-29 23:07:33 +0900

    next.config.jsを追加

---
+++ next.config.js
@@ -0,0 +1,11 @@
+// @ts-check
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  /* config options here */
+  images: {
+    domains: ["https://assets.tina.io"],
+  },
+};
+
+module.exports = nextConfig;
